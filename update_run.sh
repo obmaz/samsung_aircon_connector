bcddc50fcd9251723fa3dfcdd09ec48ede4d817c
obmaz <zambobmaz@gmail.com>
2020-10-13 21:43:10 +0900

    Fixed

--- update_run.sh
+++ update_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#git reset --hard origin/master
-#git checkout master
-#git pull
-#git fetch --tags
+git reset --hard origin/master
+git checkout master
+git pull
+git fetch --tags
 
-#go mod download
-#go build
+go mod download
+go build
 
-#pkill -9 samsung_legacy_aircon_connector
+pkill -9 samsung_legacy_aircon_connector
 ./samsung_legacy_aircon_connector
