c36f47f25e993c56df699d16fb04fbe37aa912b0
obmaz <zambombaz@gmail.com>
2021-02-02 17:00:22 +0900

    Fixed

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker build --no-cache --tag zambobmaz/samsung_legacy_aircon_connector:1.1.0 .
+sudo docker build --no-cache --tag zambobmaz/samsung_legacy_aircon_connector:1.2.0 .
