45ce25785ee7df703ac9c58c2994647969915247
obmaz <zambombaz@gmail.com>
2021-02-04 15:56:35 +0900

    Fixed

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo cp -n /app/samsung_legacy_aircon_connector/config/config.yaml /config
-/app/samsung_legacy_aircon_connector/run.sh
+#sudo cp -n /app/samsung_legacy_aircon_connector/config/config.yaml /config
+/app/run.sh
