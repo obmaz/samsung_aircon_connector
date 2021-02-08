74d1cf38b1a68f4cbbe92c307fe7d2dd97c1da34
obmaz <zambombaz@gmail.com>
2021-02-08 18:03:31 +0900

    Fixed

--- docker_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker run -d --restart always -p 20080:20080 --name samsung_aircon_connector -v /volume1/docker/samsung_legacy_aircon_connector/config:/config zambobmaz/samsung_legacy_aircon_connector
