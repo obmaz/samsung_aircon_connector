ac702a1d7c62070dc7a48e2050cbcb8fddefcb90
obmaz <zambobmaz@gmail.com>
2021-07-01 18:04:51 +0900

    Fixed

--- devicetypes/imageafter45121/af-ha153.src/makePresentation.sh
+++ devicetypes/imageafter45121/af-ha153.src/makePresentation.sh
@@ -15,10 +15,9 @@
 # ./smartthings capabilities:presentation:create imageafter45121.colorTemperatureMoon 1 -y -i=colorTemperature.json
 
 # Create deviceConfig.json
-# The dth uid can be found on groovy ide url / af ha153 dth key : 86e1ea6e-d44a-4737-b721-c4d547f65fee
+# The dth uid can be found on groovy ide url / af ha153 dth key : 12a5ef77-4ab3-4f02-af41-3ad5b4cd7ce6
 # It makes the deviceConfig.json based on dth
-#./smartthings presentation:device-config:generate 86e1ea6e-d44a-4737-b721-c4d547f65fee --dth -o=deviceConfig.json -j
-
+#./smartthings presentation:device-config:generate 12a5ef77-4ab3-4f02-af41-3ad5b4cd7ce6 --dth -o=deviceConfig.json -j
 
 # Create vid
 # it makes the vid based on deviceConfig.json
