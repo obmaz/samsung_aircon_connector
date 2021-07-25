a701460b4127f9615dbd085863abe603d174c016
zambobmaz <zambobmaz@gmail.com>
2021-07-25 20:49:36 +0900

    Fixed

--- devicetypes/imageafter45121/af-ha153.src/af-ha153.groovy
+++ devicetypes/imageafter45121/af-ha153.src/af-ha153.groovy
@@ -33,7 +33,7 @@ import groovy.transform.Field
 @Field Map currentState = [:]
 
 metadata {
-    definition(name: "af ha153", namespace: "imageafter45121", author: "obmaz", mnmn: "SmartThingsCommunity", vid: "92df687c-82d2-335d-9040-5c6dff7fe53c", ocfDeviceType: 'oic.d.airconditioner') {
+    definition(name: "af ha153", namespace: "imageafter45121", author: "obmaz", mnmn: "SmartThingsCommunity", vid: "7058efc0-258b-3f62-8500-062816ef1bf7", ocfDeviceType: 'oic.d.airconditioner') {
         capability "Switch"
         capability "Temperature Measurement"
         capability "imageafter45121.acTempSet"
