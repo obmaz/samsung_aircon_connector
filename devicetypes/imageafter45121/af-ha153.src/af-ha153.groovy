07e05e57d826e0ca5f248b09a7ae6db56fef886c
obmaz <zambobmaz@gmail.com>
2022-07-04 14:28:33 +0900

    Change stepper to slide due to stepper bug of smarthings

--- devicetypes/imageafter45121/af-ha153.src/af-ha153.groovy
+++ devicetypes/imageafter45121/af-ha153.src/af-ha153.groovy
@@ -30,7 +30,7 @@
 import groovy.json.JsonSlurper
 
 metadata {
-    definition(name: "af ha153", namespace: "imageafter45121", author: "obmaz", mnmn: "SmartThingsCommunity", vid: "0694508d-8d01-3e09-857c-ccba143c53b9", ocfDeviceType: 'oic.d.airconditioner') {
+    definition(name: "af ha153", namespace: "imageafter45121", author: "obmaz", mnmn: "SmartThingsCommunity", vid: "03d738d2b-fadf-3275-9dac-eceba624bb5", ocfDeviceType: 'oic.d.airconditioner') {
         capability "Switch"
         capability "Temperature Measurement"
         capability "imageafter45121.acTempSet"
