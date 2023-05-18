39bac45bf7ca5596e3f226d18f83f7f2f3488c39
obmaz <zambobmaz@gmail.com>
2023-05-18 11:24:53 +0900

    Misc

--- edge/install.sh
+++ edge/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-driverId=598332a7-775e-45cc-bb9c-246bff3041a1
+driverId=927b7213-86cf-423e-9ee6-006b02f7e1e3
 channel=699fefe6-7b99-40b2-acfd-662ed510a84d
 hub=37d997a3-7579-47f2-8ae9-804fce729f7b
 hub_address=192.168.0.119
